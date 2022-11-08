47e349cfee4ce0f18ab2153d73de1888e156400b
Damon L Montague Jr <damon@montague.im>
2022-11-08 00:47:42 -0500

    Added logging for request, added test predict endpoint

--- API/src/main.rs
+++ API/src/main.rs
@@ -1,19 +1,27 @@
-use actix_web::{middleware, post, web, App, Error, HttpResponse, HttpServer};
+use actix_web::{App, HttpServer, web};
+use actix_web::middleware::Logger;
+use env_logger::Env;
+
+
+mod routes;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-
     let endpoint = format!("0.0.0.0:{}", 8080);
     println!("Running server at {}", endpoint);
 
+    env_logger::init_from_env(Env::default().default_filter_or("info"));
+
     HttpServer::new(move || {
         App::new()
-            .wrap(middleware::Logger::default())
+            .wrap(Logger::default())
+            .service(
+                web::scope("/sports")
+                            .route("/predict", web::post().to(routes::nn::predict))
+            )
+
     })
         .bind(endpoint)?
         .run()
         .await
-
-
-
 }
