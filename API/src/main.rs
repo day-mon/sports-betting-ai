301a954ae9b2afd0d1c6a94f7cb1cc0ebb1727fe
Damon L Montague Jr <damon@montague.im>
2022-11-07 23:32:28 -0500

    Added actix-web to the project

--- API/src/main.rs
+++ API/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{middleware, post, web, App, Error, HttpResponse, HttpServer};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+
+    let endpoint = format!("0.0.0.0:{}", 8080);
+    println!("Running server at {}", endpoint);
+
+    HttpServer::new(move || {
+        App::new()
+            .wrap(middleware::Logger::default())
+    })
+        .bind(endpoint)?
+        .run()
+        .await
+
+
+
 }
