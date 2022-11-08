64f46d1da969a5428294a061961c16b76bf8dc90
Damon L Montague Jr <damon@montague.im>
2022-11-08 01:28:48 -0500

    Fixed duplicate models

--- API/src/routes/nn_mock.rs
+++ API/src/routes/nn_mock.rs
@@ -1,15 +1,8 @@
 use actix_web::{HttpResponse, web};
 use actix_web::http::StatusCode;
 use serde::{Deserialize, Serialize};
+use crate::routes::nn::PredictModel;
 
-#[derive(Deserialize, Serialize)]
-pub struct PredictModel {
-    team_one: String,
-    team_two: String,
-    over_under: f32,
-    team_one_odds: f32,
-    team_two_odds: f32
-}
 
 pub async fn predict(
     model: web::Json<PredictModel>
