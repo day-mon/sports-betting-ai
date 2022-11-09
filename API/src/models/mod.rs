a54978971dd2bd2e85a6e79a86b0cc5c0b14843e
Connor <ces156@pitt.edu>
2022-11-09 12:41:44 -0500

    added api call for fetching TeamStats
    Took 25 minutes

--- API/src/models/mod.rs
+++ API/src/models/mod.rs
@@ -2,3 +2,4 @@ pub mod prediction;
 pub mod game;
 pub mod utils;
 pub mod daily_games;
+pub mod team_stats;
--- API/src/routes/nn.rs
+++ API/src/routes/nn.rs
@@ -1,8 +1,11 @@
 use actix_web::{HttpResponse, web};
 use crate::models::daily_games::{DailyGames, Match};
 use crate::models::game::Game;
+use crate::models::team_stats::TeamStats;
 
 const DAILY_GAMES_URL: &str = "https://data.nba.com/data/v2015/json/mobile_teams/nba/2022/scores/00_todays_scores.json";
+const TEAM_DATA_URL: &str = "https://upload.montague.im/r/BBOePC.json";
+
 
 pub async fn predict(
     // model: web::Json<Game>
@@ -23,7 +26,24 @@ pub async fn predict(
         return HttpResponse::NotFound().json("There are no games today")
     }
 
-    let tids: Vec<Match> = daily_games.gs.g.iter().map(|g| Match { home_team_id: g.h.tid, away_team_id: g.v.tid } ).collect();
+    let response = match reqwest::get(TEAM_DATA_URL).await {
+        Ok(res) => res,
+        Err(_) => { return HttpResponse::InternalServerError().json("Booo") }
+    };
 
-    HttpResponse::Ok().json(daily_games)
+    let response_body = match response.text().await {
+        Ok(body) => body,
+        Err(_) => { return HttpResponse::InternalServerError().json("boo v2") }
+    };
+
+    let team_stats: TeamStats = match serde_json::from_str(&*response_body) {
+        Ok(body) => body,
+        Err(_) => { return HttpResponse::InternalServerError().json("Failed to parse response from damon.com") }
+    };
+
+    println!("team_stats: {:?}", team_stats);
+
+
+    let tids: Vec<Match> = daily_games.gs.g.iter().map(|g| Match { home_team_id: g.h.tid, away_team_id: g.v.tid } ).collect();
+    HttpResponse::Ok().json(team_stats)
 }
