93949bc4698bac8cf528d07cd5b91748f484e08a
damon <damon@montague.im>
2022-11-09 16:14:04 -0500

    wip getting predict to work

--- API/src/routes/nn.rs
+++ API/src/routes/nn.rs
@@ -1,4 +1,6 @@
+use std::collections::HashMap;
 use actix_web::{HttpResponse, web};
+use serde_json::Value;
 use crate::models::daily_games::{DailyGames, Match};
 use crate::models::game::Game;
 use crate::models::team_stats::TeamStats;
@@ -10,40 +12,39 @@ const TEAM_DATA_URL: &str = "https://upload.montague.im/r/BBOePC.json";
 pub async fn predict(
     // model: web::Json<Game>
 ) -> HttpResponse {
-
-  let response = match reqwest::get(DAILY_GAMES_URL).await {
-      Ok(res) => res,
-      Err(_) => { return HttpResponse::InternalServerError().json("Booo") }
-  };
-
-    let response_body = match response.text().await {
-        Ok(body) => body,
-        Err(_) => { return HttpResponse::InternalServerError().json("boo v2") }
+    let Ok(response) =  reqwest::get(DAILY_GAMES_URL).await else {
+        return HttpResponse::InternalServerError().json("Booo")
     };
-    let daily_games: DailyGames = serde_json::from_str(&*response_body).unwrap();
 
+    let Ok(response_body) = response.text().await else {
+      return HttpResponse::InternalServerError().json("boo v2");
+    };
+
+    let daily_games: DailyGames = serde_json::from_str(&*response_body).unwrap();
     if daily_games.gs.g.is_empty() {
-        return HttpResponse::NotFound().json("There are no games today")
+        return HttpResponse::NotFound().json("There are no games today");
     }
 
-    let response = match reqwest::get(TEAM_DATA_URL).await {
-        Ok(res) => res,
-        Err(_) => { return HttpResponse::InternalServerError().json("Booo") }
+    let tids: Vec<Match> = daily_games.gs.g.iter().map(|g| Match { home_team_id: g.h.tid, away_team_id: g.v.tid }).collect();
+
+    let Ok(response) =  reqwest::get(TEAM_DATA_URL).await else {
+       return HttpResponse::InternalServerError().json("Booo");
     };
 
-    let response_body = match response.text().await {
-        Ok(body) => body,
-        Err(_) => { return HttpResponse::InternalServerError().json("boo v2") }
+    let Ok(response_body) = response.text().await else {
+         return HttpResponse::InternalServerError().json("boo v2");
     };
 
-    let team_stats: TeamStats = match serde_json::from_str(&*response_body) {
-        Ok(body) => body,
-        Err(_) => { return HttpResponse::InternalServerError().json("Failed to parse response from damon.com") }
-    };
+    let team_stats: TeamStats = serde_json::from_str(&*response_body).unwrap();
 
-    println!("team_stats: {:?}", team_stats);
+    let mut team_stats_map: HashMap<i64, &Vec<Vec<Value>>> = HashMap::new();
+    for set in team_stats.result_sets.into_iter() {
+        let stats = set.row_set[0].clone();
+        println!("{:?}", stats)
+    }
 
 
-    let tids: Vec<Match> = daily_games.gs.g.iter().map(|g| Match { home_team_id: g.h.tid, away_team_id: g.v.tid } ).collect();
+
+
     HttpResponse::Ok().json(team_stats)
 }
