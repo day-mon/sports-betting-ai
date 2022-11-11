b9a7b475c3424f462a2a51a9e60ce9d4a7782d4b
Damon L Montague Jr <damon@montague.im>
2022-11-11 02:17:21 -0500

    Fixed broken data url, made it so we always dont write to CSV

--- API/src/routes/nn.rs
+++ API/src/routes/nn.rs
@@ -1,67 +1,95 @@
+use std::fs;
 use std::fs::File;
-use std::sync::Arc;
-
 use actix_web::HttpResponse;
-use polars::error::PolarsResult;
 use polars::frame::DataFrame;
 use polars::io::SerReader;
-use polars::prelude::{CsvReader, Schema};
+use polars::prelude::CsvReader;
 //use tensorflow::{DataType, Graph, SavedModelBundle, Session, SessionOptions, Tensor};
 use crate::models::daily_games::{DailyGames, Match};
 use crate::models::team_stats::TeamStats;
 
 
 const DAILY_GAMES_URL: &str = "https://data.nba.com/data/v2015/json/mobile_teams/nba/2022/scores/00_todays_scores.json";
-const TEAM_DATA_URL: &str = "https://jsonget.alexanderjoemc.workers.dev/";
+const TEAM_DATA_URL: &str = "https://lively-fire-943d.alexanderjoemc.workers.dev/";
 
 
 pub async fn predict(
     // model: web::Json<Game>
 ) -> HttpResponse {
-    let Ok(response) =  reqwest::get(DAILY_GAMES_URL).await else {
-        return HttpResponse::InternalServerError().json("Booo")
+    let Ok(response) = reqwest::get(DAILY_GAMES_URL).await else {
+        return HttpResponse::InternalServerError().json("Booo");
     };
 
     let Ok(response_body) = response.text().await else {
-      return HttpResponse::InternalServerError().json("boo v2");
+        return HttpResponse::InternalServerError().json("boo v2");
     };
 
-    let daily_games: DailyGames = serde_json::from_str(&*response_body).unwrap();
+
+    let Ok(daily_games) = serde_json::from_str::<DailyGames>(&*response_body) else {
+        return HttpResponse::InternalServerError().json("boo v3");
+    };
+
+
     if daily_games.gs.g.is_empty() {
         return HttpResponse::NotFound().json("There are no games today");
     }
-
+    let date = daily_games.gs.gdte.to_string().replace("\"", "");
     let tids: Vec<Match> = daily_games.gs.g.iter().map(|g| Match { home_team_id: g.h.tid, away_team_id: g.v.tid }).collect();
 
-    let Ok(response) = reqwest::get(TEAM_DATA_URL).await else {
-       return HttpResponse::InternalServerError().json("Booo");
+
+    let model_data = match get_model_data(&tids, &date).await  {
+        Ok(data) => data,
+        Err(e) => return HttpResponse::InternalServerError().json(e.to_string())
     };
 
+
+    HttpResponse::Ok().json("daily_stats")
+}
+
+
+async fn get_model_data(matches: &Vec<Match>, date: &String) -> Result<DataFrame, String> {
+    let file_name = format!("./src/data/{}.csv", date);
+    // check if file exist if it exist return the string of the file
+    if let Ok(file) = File::open(file_name) {
+        // convert string to dataframe
+         return Ok(CsvReader::new(file)
+            .infer_schema(Some(100))
+            .has_header(true)
+            .finish()
+            .expect("Unable to read csv"));
+    };
+
+
+    let Ok(response) = reqwest::get(TEAM_DATA_URL).await else {
+        return Err("Error fetching team data".to_owned());
+    };
 
     let Ok(response_body) = response.text().await else {
-         return HttpResponse::InternalServerError().json("boo v2");
+        return Err("Error getting response body from team data source".to_owned());
     };
 
-    let team_stats: TeamStats = serde_json::from_str(&*response_body).unwrap();
+    let Ok(daily_stats) = serde_json::from_str::<TeamStats>(&*response_body) else {
+        return Err("Error deserializing team data".to_owned());
+    };
 
-    // figure out a way not to always write to csv. We only need to write to a csv everyday
-    let written = write_to_csv(&tids, &team_stats);
+    let written  = match write_to_csv(matches, &daily_stats, date) {
+        Ok(written) => written,
+        Err(e) => return Err(e),
+    };
 
-    if !written {
-        return HttpResponse::InternalServerError().json("Failed parse data to use for model");
-    }
-
-
-    call_model(&team_stats);
-
-
-    HttpResponse::Ok().json("stats")
+   // convert written string to dataframe
+    return Ok(CsvReader::new(written)
+        .infer_schema(Some(100))
+        .has_header(true)
+        .finish()
+        .expect("Unable to read csv"));
 }
+
 
 // write to a csv and put the two teams that are playing against each other in the same row
 // this is so we can use the csv as input to the model
 // return a bool if the csv was written successfully
-fn write_to_csv(matches: &Vec<Match>, team_stats: &TeamStats) -> bool {
+fn write_to_csv(matches: &Vec<Match>, team_stats: &TeamStats, date: &String) -> Result<File, String> {
     let mut csv = String::new();
     for i in 0..2
     {
@@ -77,11 +105,14 @@ fn write_to_csv(matches: &Vec<Match>, team_stats: &TeamStats) -> bool {
         for team in teams
         {
             let csv_data: String = team_stats.result_sets[0].get_team_stats(team)
-                              .iter()
-                              .map(|val| val.iter().map(|val| val.to_string()).collect::<Vec<String>>().join(","))
-                              .collect();
+                .iter()
+                .map(|val| val.iter().map(|val| val.to_string()).collect::<Vec<String>>().join(","))
+                .collect();
 
-            if csv_data.is_empty() { return false; }
+            if csv_data.is_empty() {
+                return Err("Failed to find team stats".to_owned());
+
+            }
 
             csv.push_str(csv_data.as_str());
             if team == mat.home_team_id { csv.push_str(","); }
@@ -89,8 +120,13 @@ fn write_to_csv(matches: &Vec<Match>, team_stats: &TeamStats) -> bool {
         csv.push_str("\n");
     }
 
-    let written= std::fs::write("data.csv", csv);
-    written.is_ok()
+
+    let written = fs::write( format!("./src/data/{}.csv", date), csv);
+    if written.is_err() {
+        return Err("Failed to write to csv".to_owned());
+    } else {
+        Ok(File::open(format!("./src/data/{}.csv", date)).unwrap())
+    }
 }
 
 fn call_model(stats: &TeamStats) {
@@ -113,28 +149,4 @@ fn call_model(stats: &TeamStats) {
 
     println!("{:?}", df.is_ok());
 }
-/*
-fn df_to_tensor(df: DataFrame)  {
-    let (nrows, ncols) = df.shape();
-    let mut data = vec![0.0; nrows * ncols];
 
-
-    // drop all values from dataframe above
-    let df = df.drop("Unnamed: 0").unwrap();
-    let df = df.drop("Score").unwrap();
-    let df = df.drop("GP").unwrap();
-    let df = df.drop("GP.1").unwrap();
-    let df = df.drop("Home-Team-Win").unwrap();
-    let df = df.drop("TEAM_NAME").unwrap();
-    let df = df.drop("Date").unwrap();
-    let df = df.drop("MIN").unwrap();
-    let df = df.drop("MIN.1").unwrap();
-    let df = df.drop("TEAM_NAME.1").unwrap();
-    let df = df.drop("Date.1").unwrap();
-    let df = df.drop("OU").unwrap();
-    let df = df.drop("OU-Cover").unwrap();
-    let df = df.drop("GP_RANK").unwrap();
-    let df = df.drop("GP_RANK.1").unwrap();
-
-}
-*/
