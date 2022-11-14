8119fbac62b362dc73e158cc46332594b333cb1e
Alexander <alexanderjoemc@gmail.com>
2022-11-14 16:22:18 -0500

    setup tailwind and basic pages

---
+++ Website/src/pages/Home.tsx
@@ -0,0 +1,9 @@
+import type { Component } from 'solid-js';
+
+const Home: Component = () => {
+  return (
+    <h1 class='text-2xl text-amber-300'>Hello Home Page!</h1>
+  );
+};
+
+export default Home;
