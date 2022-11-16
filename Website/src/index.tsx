51e50c805c88019ffc733097538177e309cbb0d1
Damon L Montague Jr <damon@montague.im>
2022-11-16 16:31:44 -0500

    Added router with all pages
    Took 27 minutes

--- Website/src/index.tsx
+++ Website/src/index.tsx
@@ -1,7 +1,12 @@
-/* @refresh reload */
-import { render } from 'solid-js/web';
+import { render } from "solid-js/web";
+import { Router } from "@solidjs/router";
+import App from "./App";
 
-import './index.css';
-import App from './App';
-
-render(() => <App />, document.getElementById('root') as HTMLElement);
+render(
+    () => (
+        <Router>
+            <App />
+        </Router>
+    ),
+    document.getElementById("root") as HTMLElement
+);
---
+++ Website/src/pages/Home.tsx
@@ -0,0 +1,6 @@
+export default function Home() {
+    return <div>
+        <h1>Home</h1>
+        <p>Welcome to my site.</p>
+    </div>
+}
