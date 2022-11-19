1cf1c0816a10b841bbe575241132e2bfa9d99984
Alexander <alexanderjoemc@gmail.com>
2022-11-19 14:22:49 -0500

    remove gap at bottom of page, change card color

--- Website/src/components/Card.tsx
+++ Website/src/components/Card.tsx
@@ -12,8 +12,8 @@ export const Card: Component<IBetCards> = (
 ) => {
     let game = props.game;
     return (
-        <div class="max-2xl p-4 bg-gray-700/75 border gre rounded-lg shadow-md hover:bg-gray-100 dark:bg-amber-500 dark:border-gray-700 dark:hover:bg-amber-500 mb-4">
-            <h5 class="mb-1 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{game.away_team_name} vs {game.home_team_name}</h5>
+        <div class="max-2xl p-4 border border-gray-500 rounded-lg shadow-md mb-4 bg-gray-800 hover:bg-gray-700 text-white">
+            <h5 class="mb-1 text-2xl font-bold tracking-tight text-white">{game.away_team_name} vs {game.home_team_name}</h5>
             <Menu
                 as={'dropdown'}
                 class={"flex flex-col"}
