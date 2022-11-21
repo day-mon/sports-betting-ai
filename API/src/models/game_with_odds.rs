1a071dae007e3c2854468cddaa6190b269966653
Damon L Montague Jr <damon@montague.im>
2022-11-20 21:19:17 -0500

    Added score
    Took 7 minutes

--- API/src/models/game_with_odds.rs
+++ API/src/models/game_with_odds.rs
@@ -1,6 +1,6 @@
 use serde::{Deserialize, Serialize};
 use crate::{util::string::remove_quotes, models::daily_games::G};
-use crate::models::game_odds::Book;
+
 
 
 #[derive(Deserialize, Serialize)]
@@ -8,7 +8,9 @@ pub struct GameWithOdds {
     pub game_id: String,
     pub start_time: String,
     pub home_team_name: String,
+    pub home_team_score: String,
     pub away_team_name: String,
+    pub away_team_score: String,
     pub home_team_id: i64,
     pub away_team_id: i64,
     pub odds: Vec<Odds>
@@ -23,6 +25,8 @@ impl GameWithOdds {
             away_team_name: format!("{} {}", remove_quotes(&match_up.v.tc), remove_quotes(&match_up.v.tn)),
             home_team_id: match_up.h.tid,
             away_team_id: match_up.v.tid,
+            home_team_score: remove_quotes(&match_up.h.s),
+            away_team_score: remove_quotes(&match_up.v.s),
             odds: Vec::new()
         }
     }
