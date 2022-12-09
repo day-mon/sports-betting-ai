d7b30e66938242876f7ba9395430afb85e000163
Damon L Montague Jr <damon@montague.im>
2022-12-09 18:15:54 -0500

    Fixed dockerfile for new name changes, Added OU model, Changed trained models location because old folder naming was confusing, Added more parameters to return struct for predictions
    Took 1 hour 32 minutes

--- API/src/models/prediction.rs
+++ API/src/models/prediction.rs
@@ -3,6 +3,8 @@ use {serde::{Deserialize, Serialize}};
 
 #[derive(Deserialize, Serialize, Clone)]
 pub struct Prediction {
-    pub predicted_winner: String,
-    pub game_id: String
+    pub prediction_type: String,
+    pub prediction: String,
+    pub game_id: String,
+    pub confidence: Option<f32>
 }
