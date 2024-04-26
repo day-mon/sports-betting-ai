5d360dc4879f698b39badcc9d30ed562c63e61ad
Alexander <alexanderjoemc@gmail.com>
2024-04-26 00:39:55 -0400

    add some animation to home, placeholder for history

--- ui/src/pages/History.tsx
+++ ui/src/pages/History.tsx
@@ -1,3 +1,9 @@
 export const History = () => {
-  return <></>;
+  return (
+    <>
+      <main class="pt-4 min-h-screen bg-primary">
+        <h1 class="text-3xl text-center text-white">History</h1>
+      </main>
+    </>
+  );
 };
