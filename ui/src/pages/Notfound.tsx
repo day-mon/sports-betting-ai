b804267adb268644d05aad27d0f5e3fbbdefbe3f
Damon L. Montague Jr. <damon@montague.im>
2024-04-28 17:30:21 -0400

    updated 404 page

--- ui/src/pages/Notfound.tsx
+++ ui/src/pages/Notfound.tsx
@@ -1,11 +1,10 @@
-import { Component, createSignal, Show } from 'solid-js';
+import { Component, createSignal } from 'solid-js';
 import { AnimationDiv } from '~/components/animated-div.tsx';
 import { useLocation } from '@solidjs/router';
 
 export const Notfound: Component = () => {
 
   const [text, setText] = createSignal<string>('')
-  const [showOther, setShowOther] = createSignal<boolean>(false)
   let index = 0
   const location = useLocation()
 
@@ -14,17 +13,17 @@ export const Notfound: Component = () => {
     setText((t) => t + message[index++])
     if (index >= message.length) {
       clearInterval(interval)
-      setShowOther(true)
     }
   }, 100)
   return (
-    <AnimationDiv class={'flex flex-col justify-center bg-primary items-center h-screen'}>
-      <h1 class={'text-9xl flex flex-row font-bold text-100'}>{text()}</h1>
-      <Show when={showOther()}>
-        <h1 class={'text-3xl text-100'}>The page at {location.pathname} does not exist.</h1>
-      </Show>
+    <AnimationDiv class={'flex flex-col justify-center bg-primary items-center h-screen animate-pulse'}>
+      <h1 class={'text-9xl flex flex-row font-bold text-100 animate-bounce'}>{text()}</h1>
+      <AnimationDiv class={'mt-5'}>
+        <h1 class={'text-3xl text-100 animate-fade-in-down'}>I think... you're lost
+          <strong class={'mx-2 text-red-500'}>{location.pathname}</strong>
+          does not exist.
+        </h1>
+      </AnimationDiv>
     </AnimationDiv>
   );
 };
-
-
